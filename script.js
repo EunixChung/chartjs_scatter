9f7f3e63480e9b877096322128faf8d63f3c93df
EunixChung <46728392+EunixChung@users.noreply.github.com>
2021-02-17 00:12:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
   var POINT_Y_POSTFIX = ''; // postfix for x values, eg ' kg'
 
   var POINT_NAME = 'district'; // point names that appear in tooltip
-  var POINT_COLOR = 'rgba(0,0,255,0.7)'; // eg `black` or `rgba(10,100,44,0.8)`
-  var POINT_RADIUS = 5; // radius of each data point
+  var POINT_COLOR = 'rgba(0,0,255,0.8)'; // eg `black` or `rgba(10,100,44,0.8)`
+  var POINT_RADIUS = 4; // radius of each data point
 
   var X_AXIS = 'Median Household Income, USD'; // x-axis label, label in tooltip
   var Y_AXIS = 'Grade, Relative to Average'; // y-axis label, label in tooltip
